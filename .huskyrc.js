68af25d6c88f8f9364339701a6bf90608fc4a5a8
liuxiajiang <liuxiajiang@100tal.com>
2019-09-24 11:53:23 +0800

    feat: english test
    详情再次来临

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,11 @@
+const tasks = arr => arr.join(' && ')
+
 module.exports = {
   hooks:{
-    "commit-msg": "validate-commit-msg"
+    "commit-msg": "validate-commit-msg",
+    "post-commit": tasks([
+      'echo generator changelog',
+      'npm run changelog'
+    ])
   }
 }
